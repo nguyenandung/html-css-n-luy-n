b0862987816a7cc31acf0d7f6fd5fb7f9055fbfc
nguyenandung <txo432@gmail.com>
2021-12-12 21:12:00 +0700

    upload

---
+++ 1/1.js
@@ -0,0 +1,7 @@
+var a =5
+
+function chaythu() {
+	a=a+1
+	console.log('gia tri cua b la: ' +((a%5)+1))
+
+}
---
+++ 2/2.js
@@ -0,0 +1,7 @@
+var b=5
+console.log(b)
+if(b==4) {
+	console.log("hello");
+}else {
+	console.log("cho")
+}
