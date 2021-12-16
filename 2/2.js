74f0c8ddcc737726cc4f492660b5a018aa8e7edb
nguyenandung <txo432@gmail.com>
2021-12-16 16:31:36 +0700

    upload

--- 2/2.js
+++ 2/2.js
@@ -1,7 +1,15 @@
-var b=5
-console.log(b)
-if(b==4) {
-	console.log("hello");
-}else {
-	console.log("cho")
+var hocvien = {
+	ten : "Viet",
+	ho : "Nguyen",
+	tuoi: 31,
+	chieucao:197,
+	cannang:45,
+	tendaydu: function () {
+		return this.ho +" " + this.ten ;},
+	danhgia: function () {
+		if (this.chieucao/this.cannang < 3) {return "đep trai" ;}
+		else {return "xau xí";}
+	}
 }
+console.log(hocvien.tendaydu());
+console.log(hocvien.danhgia())
---
+++ c++/bai1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b;
+	int s=0;
+	cout<<"nhap gia tri a:\n";
+	cin>>a;
+	cout<<"nhap gia trib:\n";
+	cin>>b;
+	s=a+b;
+	cout<<"s="<<s;
+	return 0;
+}
